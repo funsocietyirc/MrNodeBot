2f2a28378f03c1c97870e0c02ab47d11dafdd7f6
Dave Richer <Dave@Develops.io>
2016-08-10 07:39:00 -0400

    landing page

--- scripts/pages/static.js
+++ scripts/pages/static.js
@@ -4,6 +4,8 @@ module.exports = app => {
     // Register upload Handler
     app.WebRoutes.set('landingPage', {
         handler: (req,res) => {
+            let ip =  req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+            console.log(`Landing page has been visited by: ${ip}` )
             res.render('landing', {});
         },
         desc: 'Landing Page',
