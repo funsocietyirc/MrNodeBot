14f3192a14d6438a6daa62ca28de4b16c324ea9e
Dave Richer <dave@develops.io>
2016-09-28 21:54:50 -0400

    werd

--- scripts/modules/images.js
+++ scripts/modules/images.js
@@ -62,7 +62,7 @@ module.exports = app => {
                                 });
                             });
                         });
-                    };
+                    });
                 };
 
             // Web Front End
