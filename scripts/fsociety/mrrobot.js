619becc6b84de6a24e8253aa8d944e0e84ba18ea
Dave Richer <dave@develops.io>
2016-10-02 20:37:28 -0400

    show quote cleanup and scheduler

--- scripts/fsociety/mrrobot.js
+++ scripts/fsociety/mrrobot.js
@@ -20,63 +20,65 @@ module.exports = app => {
         'i am Mr. Robot (~mrrobot@unaffiliated/kl4200/bot/mrrobot)',
         'Error:',
         'Get a random fact from the database of weird facts',
+        'I\'m now ignoring you for 5 minutes.',
         'I don\'t recognize you. You can message me either of these two commands:',
         'invalid commands within the last',
         'Quote #'
     ];
 
     const mrRobotQuoteLogging = (to, from, text, message) => {
-        if (!text || to != '#MrRobot' || from != 'MrRobot' ||  _.includes(includeExceptions, text) || text.split(' ').length < 3) {
+        if (!text || to != '#MrRobot' || from != 'MrRobot' || _.includes(includeExceptions, text) || text.split(' ').length < 3) {
             return;
         }
         quoteModel.query(qb => {
-           qb
-           .select(['quote'])
-           .where('quote', 'like', text)
-           .limit(1);
+                qb
+                    .select(['quote'])
+                    .where('quote', 'like', text)
+                    .limit(1);
 
-        })
-        .fetch()
-        .then(result => {
-          if(result) {
-            return;
-          }
-          quoteModel
-            .insert({quote:text})
+            })
+            .fetch()
             .then(result => {
+                if (result) {
+                    return;
+                }
+                quoteModel
+                    .insert({
+                        quote: text
+                    })
+                    .then(result => {})
+                    .catch(err => {
+                        conLogger('Error saving result from DB in MrRobotQuote', 'error');
+                        conLogger(err, 'error');
+                    });
             })
             .catch(err => {
-              conLogger('Error saving result from DB in MrRobotQuote','error');
-              conLogger(err, 'error');
+                conLogger('Error getting result from DB in MrRobotQuote', 'error');
+                conLogger(err, 'error');
             });
-        })
-        .catch(err => {
-          conLogger('Error getting result from DB in MrRobotQuote','error');
-          conLogger(err, 'error');
-        });
     };
 
     const cleanQuotes = (to, from, text, mesasage) => {
-      quoteModel.query(qb => {
-        qb.where('quote', 'like', '%1 more message%')
-          .select(['id', 'quote']);
-      })
-      .fetchAll()
-      .then(results => {
-        if(!results.count()) {
-          return;
-        }
-        results.forEach(result => {
-          quoteModel
-            .where('id',result.attributes.id + 1)
-            .fetch()
-            .then(secondLine => {
-              result.attributes.quote = `${result.attributes.quote.replace('(1 more message)','')} ${secondLine.attributes.quote}`;
-              result.save();
-              secondLine.destroy();
+        quoteModel.query(qb => {
+                qb.where('quote', 'like', '%(1 more message)%')
+                    .select(['id', 'quote']);
             })
-        })
-      });
+            .fetchAll()
+            .then(results => {
+                if (!results.count()) {
+                    return;
+                }
+                results.forEach(result => {
+                    quoteModel
+                        .where('id', result.attributes.id + 1)
+                        .fetch()
+                        .then(secondLine => {
+                            result.attributes.quote = `${result.attributes.quote.replace('(1 more message)','')} ${secondLine.attributes.quote}`;
+                            result.save();
+                            secondLine.destroy();
+                        })
+                })
+            });
     };
 
     const mrrobot = (to, from, text, message) => {
@@ -102,6 +104,12 @@ module.exports = app => {
         access: app.Config.accessLevels.owner,
         call: cleanQuotes
     });
+
+    // Schedule Job every hour to verify images still exist
+    app.Scheduler.scheduleJob({
+        hour: 23
+    }, cleanQuotes);
+
 
     app.Commands.set('mrrobot', {
         desc: '[Search Text] Mr Robot quotes powered by #MrRobot',
