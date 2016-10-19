a9a2a3f2d40a06b5de0dcfb3a773fe2cea7f9942
Dave Richer <dave@develops.io>
2016-10-19 03:10:12 -0400

    sent

---
+++ scripts/modules/watsonAlcamey.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const scriptInfo = {
+    name: 'Waston Alcamey',
+    file: 'watsonAlcamey.js',
+    desc: 'Test Script for watson analytics',
+    createdBy: 'Dave Richer'
+};
+
+const AlchemyLanguageV1 = require('watson-developer-cloud/alchemy-language/v1');
+
+const Models = require('bookshelf-model-loader');
+
+const _ = require('lodash');
+
+module.exports = app => {
+
+
+    const sentiment = (to, from, text, message) => {
+        // Make sure we have everything we need
+        if (!app.Config.apiKeys.watson.alchemy || !app.Config.apiKeys.watson.alchemy.apikey || !app.Database || !Models.Logging) {
+            return;
+        }
+
+        const aL = new AlchemyLanguageV1({
+            headers: {
+                'X-Watson-Learning-Opt-Out': '1'
+            },
+            showSourceText: 1,
+            api_key: app.Config.apiKeys.watson.alchemy.apikey
+        });
+
+        let textArray = text.split(' ');
+        let [nick, channel] = textArray;
+        // No Nick Provided
+        if (!text || !nick || !channel) {
+            app.say(to, 'The Sentiment command requires a Nick and a Channel');
+            return;
+        }
+        Models.Logging.query(qb => {
+                qb
+                    .select(['text', 'timestamp'])
+                    .where('from', 'like', nick)
+                    .andWhere('to', 'like', channel)
+                    .orderBy('timestamp', 'desc')
+                    .limit(100);
+            })
+            .fetchAll()
+            .then(results => {
+                let data = _(results.pluck('text')).uniq().reverse().value();
+                if (!data) {
+                    app.say(to, 'Something went wrong completing your sentiment command');
+                    return;
+                }
+                aL.sentiment({
+                    text: data.join(' ')
+                }, (err, response) => {
+                    if (err || !response || response.status != 'OK') {
+                        app.say(to, 'Something went wrong completing your sentiment command');
+                        console.log('Sentiment Error:');
+                        if (err) {
+                            console.dir(err);
+                        }
+                        return;
+                    }
+                    console.dir(response);
+                });
+            })
+            .catch(err => {
+                console.log('Sentiment Error:');
+                console.dir(err);
+            });
+    };
+    app.Commands.set('sentiment', {
+        desc: '[Nick] [Channel] Get the sentiment information for a specified user',
+        access: app.Config.accessLevels.admin,
+        call: sentiment
+    });
+
+    const question = (to, from, text, message) => {
+        if (!text) {
+            app.say(from, 'The answer to nothing is nothing....');
+            return;
+        }
+
+    };
+
+    return scriptInfo;
+};
