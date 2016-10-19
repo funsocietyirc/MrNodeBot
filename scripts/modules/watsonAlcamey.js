f6e05d39abcd26c36a319c1ffadf2e5edbada6b1
Dave Richer <dave@develops.io>
2016-10-19 07:41:20 -0400

    Personality test

--- scripts/modules/watsonAlcamey.js
+++ scripts/modules/watsonAlcamey.js
@@ -22,91 +22,96 @@ module.exports = app => {
 
     const aL = new AlchemyLanguageV1({
         headers: {
-            'X-Watson-Learning-Opt-Out': '1'
+            'X-Watson-Learning-Opt-Out': true
         },
         api_key: app.Config.apiKeys.watson.alchemy.apikey
     });
 
 
-    const getResults = (nick, channel, limit) => Models.Logging.query(qb => {
+    const getResults = (nick, channel, limit) =>
+        Models.Logging.query(qb => {
             qb
                 .select(['text', 'timestamp'])
                 .distinct('text')
-                .where('from','like', nick)
-                .andWhere('to','like', channel)
+                .where('from', 'like', nick)
+                .andWhere('to', 'like', channel)
                 .andWhere('text', 'not like', 's/%')
                 .orderBy('timestamp', 'desc')
                 .limit(limit || 500);
         })
         .fetchAll();
 
-        // Get the users overall sentiment
-        const combined = (to, from, text, message) => {
-            let textArray = text.split(' ');
-            let [nick, channel, limit] = textArray;
-            // No Nick Provided
-            if (!text || !nick || !channel) {
-                app.say(to, 'The Combined command requires a Nick and a Channel');
-                return;
-            }
-            getResults(nick, channel, limit)
-                .then(results => {
-                    let data = _(results.pluck('text')).uniq().reverse().value();
-                    if (!data) {
+    // Get the users overall sentiment
+    const combined = (to, from, text, message) => {
+        let textArray = text.split(' ');
+        let [nick, channel, limit] = textArray;
+        // No Nick Provided
+        if (!text || !nick || !channel) {
+            app.say(to, 'The Combined command requires a Nick and a Channel');
+            return;
+        }
+        getResults(nick, channel, limit)
+            .then(results => {
+                let data = _(results.pluck('text')).uniq().reverse().value();
+                if (!data) {
+                    app.say(to, 'Something went wrong completing your combined command');
+                    return;
+                }
+                aL.combined({
+                    text: data.join('. '),
+                    //showSourceText: 1, // Show Source Text for debugging
+                    extract: 'taxonomy,concept,doc-sentiment,doc-emotion'
+                }, (err, response) => {
+                    if (err || !response || response.status != 'OK') {
                         app.say(to, 'Something went wrong completing your combined command');
+                        console.log('Sentiment Error:');
+                        if (err) {
+                            console.dir(err);
+                        }
                         return;
                     }
-                    aL.combined({
-                        text: data.join('. '),
-                        //showSourceText: 1,
-                        // emotion:1,
-                        // knowledgeGraph:1,
-                        extract: 'page-image,image-kw,feed,entity,keyword,taxonomy,concept,relation,pub-date,doc-sentiment,doc-emotion'
-                    }, (err, response) => {
-                        if (err || !response || response.status != 'OK') {
-                            app.say(to, 'Something went wrong completing your combined command');
-                            console.log('Sentiment Error:');
-                            if (err) {
-                                console.dir(err);
-                            }
-                            return;
-                        }
 
-                        // Log to console
-//                        console.log(JSON.stringify(response, null, 2));
+                    // Log to console
+                    // console.log(JSON.stringify(response, null, 2));
 
-                        let concepts = _.map(response.concepts,'text').join(', ');
-                        let taxonomy = [];
-                        _(response.taxonomy).map('label').map(value => _.filter(value.split('/')), n => true).uniq().each(value => {
-                          _.each(value, item => {
+                    // Prepare the concepts
+                    let concepts = _.map(response.concepts, 'text').join(', ');
+
+                    // Prepare the taxonomy
+                    let taxonomy = [];
+                    _(response.taxonomy).map('label').map(value => _.filter(value.split('/')), n => true).uniq().each(value => {
+                        _.each(value, item => {
                             taxonomy.push(item);
-                          });
                         });
-                        taxonomy = _.uniq(taxonomy).join(', ');
-                        
-                        let space = () => ' - ';
-                        let output = `${nick} is interested in concepts like: ${concepts}`;
-                        output = output + space() + `Most of the time ${nick} is ${response.docSentiment.type}`;
-                        output = output + space() + `They are also interested in: ${taxonomy}`;
-                        output = output + space() + `There emotional state is: `;
-                        _.each(response.docEmotions, (value, key) => {
-                            output = output + ` ${_.capitalize(key)}: ${helpers.RoundNumber(value * 100)}%`
-                        });
-
-                        app.say(to, output);
-                        //app.say(to, `${nick} is that ${response.language} character who has been mostly ${response.docSentiment.type} on ${channel}`);
                     });
-                })
-                .catch(err => {
-                    console.log('Combined Error:');
-                    console.dir(err);
+                    taxonomy = _.uniq(taxonomy).join(', ');
+
+                    // Space helper
+                    let space = () => ' - ';
+
+                    // Prepare the output
+                    let output = `${nick} is interested in concepts like: ${concepts}`;
+                    output = output + space() + `Most of the time ${nick} is ${response.docSentiment.type}`;
+                    output = output + space() + `They are also interested in: ${taxonomy}`;
+                    output = output + space() + `There emotional state is: `;
+                    _.each(response.docEmotions, (value, key) => {
+                        output = output + ` ${_.capitalize(key)}: ${helpers.RoundNumber(value * 100)}%`
+                    });
+
+                    // Report back to IRC
+                    app.say(to, output);
                 });
-        };
-        app.Commands.set('combined', {
-            desc: '[Nick] [Channel] Get the combined information for a specified user',
-            access: app.Config.accessLevels.admin,
-            call: combined
-        });
+            })
+            .catch(err => {
+                console.log('Combined Error:');
+                console.dir(err);
+            });
+    };
+    app.Commands.set('combined', {
+        desc: '[Nick] [Channel] Get the combined information for a specified user',
+        access: app.Config.accessLevels.admin,
+        call: combined
+    });
 
 
     return scriptInfo;
---
+++ scripts/modules/watsonPersonality.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const scriptInfo = {
+    name: 'Waston Personality',
+    file: 'watsonPersonality.js',
+    desc: 'Test Script for watson personality',
+    createdBy: 'Dave Richer'
+};
+
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+
+const Models = require('bookshelf-model-loader');
+const helpers = require('../../helpers');
+const moment = require('moment');
+const _ = require('lodash');
+
+module.exports = app => {
+    // Make sure we have everything we need
+    if (!app.Config.apiKeys.watson.personality || !app.Config.apiKeys.watson.personality.userName || !app.Config.apiKeys.watson.personality.password || !app.Database || !Models.Logging) {
+        return scriptInfo;
+    }
+
+    const pI = new PersonalityInsightsV3({
+        headers: {
+            'X-Watson-Learning-Opt-Out': true
+        },
+        username: app.Config.apiKeys.watson.personality.userName,
+        password: app.Config.apiKeys.watson.personality.password,
+        version_date: '2016-10-19'
+    });
+
+    const getResults = (nick) =>
+        Models.Logging.query(qb => {
+            qb
+                .where('from', 'like', nick)
+                .andWhere('text', 'not like', 's/%')
+                .orderBy('timestamp', 'desc');
+        })
+        .fetchAll();
+
+    // Get the users overall sentiment
+    const personify = (to, from, text, message) => {
+        // No Nick Provided
+        if (!text) {
+            app.say(to, 'A nick is required for the personify command');
+            return;
+        }
+
+        getResults(nick)
+            .then(results => {
+                let contentItems = [];
+                results.each(model => {
+                    contentItems.push({
+                        content: model.get('text'),
+                        id: model.get('id'),
+                        userid: model.get('from'),
+                        sourceid: model.get('to'),
+                        created: moment(model.get('timestamp')).unix(),
+                        contentType: 'text/plain',
+                        language: 'en',
+                    });
+                });
+
+                pI.profile({
+                    contentItems
+                }, (error, response) => {
+                    if (error)
+                        console.log('error:', error);
+                    else
+                        console.log(JSON.stringify(response, null, 2));
+                });
+            })
+            .catch(err => {
+                console.log('Personify Error:');
+                console.dir(err);
+            });
+    };
+    app.Commands.set('personify', {
+        desc: '[Nick] Get a detailed Personal report',
+        access: app.Config.accessLevels.owner,
+        call: combined
+    });
+
+
+    return scriptInfo;
+};
