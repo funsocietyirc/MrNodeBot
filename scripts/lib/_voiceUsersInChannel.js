5e6dff4e0debe2894a8e3eca39e3c5ec14249eb4
Dave Richer <dave@develops.io>
2016-10-27 01:23:35 -0400

    logicFix

--- scripts/lib/_voiceUsersInChannel.js
+++ scripts/lib/_voiceUsersInChannel.js
@@ -11,15 +11,15 @@ module.exports = (channel, thresh, app, options) => new Promise((resolve, reject
     }
 
     options = options || {};
-    if(_.isUndefined(options.timeUnit)) options.timeUnit = 1;
-    if(_.isUndefined(options.timeMeasure)) options.timeMeasure = 'months';
+    if (_.isUndefined(options.timeUnit)) options.timeUnit = 1;
+    if (_.isUndefined(options.timeMeasure)) options.timeMeasure = 'months';
 
     return Models.Logging
         .query(qb => qb
             .where(clause =>
-              clause
-              .where('to', 'like', channel)
-              .andWhere('timestamp','>=', moment().subtract(options.timeUnit, options.timeMeasure).format('YYYY-MM-DD HH:mm:ss'))
+                clause
+                .where('to', 'like', channel)
+                .andWhere('timestamp', '>=', moment().subtract(options.timeUnit, options.timeMeasure).format('YYYY-MM-DD HH:mm:ss'))
             )
             .orderBy('id', 'desc')
         )
@@ -29,19 +29,22 @@ module.exports = (channel, thresh, app, options) => new Promise((resolve, reject
                 resolve(`I am not in, or I am not an op in ${channel}`);
                 return;
             }
+
+            let voices = [];
             _(results.toJSON())
                 .groupBy('from')
-                .filter((v, k) => app.isInChannel(channel, k) && !app._ircClient.isOpOrVoiceInChannel(channel, k))
-                .mapKeys(v => _.first(v).from)
+                .pickBy((v, k) => app.isInChannel(channel, k) && !app._ircClient.isOpOrVoiceInChannel(channel, k))
                 .mapValues(v => v.length)
-                .map((count, nick) => {
-                    if (count >= thresh) return nick
-                })
-                .chunk(4)
+                .pickBy((v, k) => v >= thresh)
                 .each((v, k) => {
-                  console.log(v,k);
-                    setTimeout(() => app._ircClient.send('MODE', channel, '+' + 'v'.repeat(v.length), v[0], v[1] || '', v[2] || '', v[3] || ''), (1 + k) * 1000);
+                    voices.push(k);
                 });
+            _(voices)
+                .chunk(4)
+                .each((v,k) => {
+                      setTimeout(() => app._ircClient.send('MODE', channel, '+' + 'v'.repeat(v.length), v[0], v[1] || '', v[2] || '', v[3] || ''), (1 + k) * 1000);
+                });
+
             resolve(`Voices ${thresh} users on ${channel}`);
         });
 });
