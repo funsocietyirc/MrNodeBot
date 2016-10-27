a3a719a6b2c0bc5e90294d955d719c0b191fe9d2
Dave Richer <dave@develops.io>
2016-10-26 21:45:50 -0400

    werd

--- scripts/admin/voiceRegulars.js
+++ scripts/admin/voiceRegulars.js
@@ -36,16 +36,15 @@ module.exports = app => {
 
         Models.Logging
             .query(qb => qb
-                .where(clause => clause.where('to', 'like', channel))
+                .where('to', 'like', channel)
                 .orderBy('id', 'desc')
             )
             .fetchAll()
             .then(results => {
                 if (!app.isInChannel(channel) || !app._ircClient.isOpInChannel(channel)) {
-                    app.say(from, `I am not an In or an Op in ${channel}`);
+                    app.say(from, `I am not in or an Op in ${channel}`);
                     return;
                 }
-                let tCount = 1;
                 _(results.toJSON())
                     .groupBy('from')
                     .filter((v, k) => app.isInChannel(channel, k) && !app._ircClient.isOpOrVoiceInChannel(channel, k))
@@ -55,11 +54,8 @@ module.exports = app => {
                         if (count >= thresh) return nick
                     })
                     .chunk(4)
-                    .each(v => {
-                        setTimeout(() => {
-                            app._ircClient.send('MODE', channel, '+' + 'v'.repeat(v.length), v[0],v[1] || '', v[2] || '', v[3] || '');
-                        }, 1000 * tCount);
-                        tCount = tCount + 1;
+                    .each((v, k) => {
+                        setTimeout(() => app._ircClient.send('MODE', channel, '+' + 'v'.repeat(v.length), v[0], v[1] || '', v[2] || '', v[3] || ''), (1 + k) * 1000);
                     });
             });
     };
