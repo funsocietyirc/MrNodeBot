b2b724f80389003510159c4730a70b0c5e75b39b
Dave Richer <dave@develops.io>
2016-11-07 11:59:55 -0500

    remove google from shortner

--- scripts/urlListener/_getShort.js
+++ scripts/urlListener/_getShort.js
@@ -1,11 +1,11 @@
 'use strict';
 const _ = require('lodash');
-const gen = require('../generators/_googleShortUrl');
-const gen2 = require('../generators/_isGdShortUrl');
+const google = require('../generators/_googleShortUrl');
+const isgd = require('../generators/_isGdShortUrl');
 const apiKey = require('../../config').apiKeys.google;
 
 module.exports = results => !apiKey || _.isEmpty(apiKey) ? results :
-    _.sample([gen,gen2])(results.url)
+    _.sample([isgd])(results.url)
     .then(url => _.merge(results, {
         shortUrl: url
     }))
