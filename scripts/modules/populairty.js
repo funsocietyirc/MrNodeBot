351bab242688c9f1aeb64f4b57c4ccebf7272c0b
Dave Richer <dave@develops.io>
2016-11-21 03:21:30 -0500

    popularity-contest

--- scripts/modules/populairty.js
+++ scripts/modules/populairty.js
@@ -12,43 +12,88 @@ const logger = require('../../lib/logger');
 const typo = require('../lib/_ircTypography');
 
 module.exports = app => {
-  // Database not available
-  if(!Models.Upvote) return scriptInfo;
+    // Database not available
+    if (!Models.Upvote) return scriptInfo;
 
-  const popularity = (to, from, text, message) => {
-    let [nick, channel] = text.split(' ');
-    // No nick available
-    if(!nick) {
-      app.say(to, `Could you possibly give me someone to rate?`);
-      return;
-    }
+    const popularityContest = (to, from, text, message) => {
+        let [nick, channel] = text.split(' ');
 
-    Models.Upvote.query(qb => {
-      qb.where('candidate','like', nick);
-      if(channel) {
-        qb.andWhere('channel','like',channel);
-      }
-    })
-    .fetchAll()
-    .then(results => {
-      if(!results.length) {
-        app.say(to, `There is no popularity data for ${nick}`);
-        return;
-      }
-      let total = _(results.pluck('result')).sum();
-
-      app.say(to, `Popularity ${channel ? 'On ' + channel : ''} ${typo.icons.sideArrow} ${results.length} ${typo.icons.views} ${typo.icons.sideArrow} ${total} ${total > 0 ? typo.icons.happy : typo.icons.sad}`);
-    })
-    .catch(err => logger.err('Error fetching record', {err}));
-  };
-
-  // Comand to destroy images
-  app.Commands.set('popularity', {
-      desc: 'Get a users popularity',
-      access: app.Config.accessLevels.identified,
-      call: popularity
-  });
+        // default to current user if no user specified
+        nick = nick || from;
+        channel = channel || to;
 
 
-  return scriptInfo;
+        Models.Upvote.query(qb => qb
+                .select()
+                .where('candidate', 'like', nick)
+                .andWhere('channel', 'like', channel)
+                .orderBy('timestamp', 'desc')
+            )
+            .fetchAll()
+            .then(results => {
+                if (!results.length) {
+                    app.say(to, `There are no results available for ${nick} in ${channel}`);
+                    return;
+                }
+                let computed = _(results.toJSON());
+
+                // Get a list of voters
+                let voters = computed.map('voter').uniq().value();
+
+                app.say(to, `Lets take a look at who likes ${nick} on ${channel}`);
+                let output = '';
+                _.forEach(voters, (voter, key) => {
+                    let computedVoter = computed.filter(f => f.voter == voter);
+                    output = output + `[${voter}]: ${computedVoter.sumBy('result')} `;
+                });
+                app.say(to, output);
+
+            })
+            .catch(err => logger.error('Error in whoLikes', {
+                err
+            }));
+    };
+    app.Commands.set('popularity-contest', {
+        desc: '[user?] [channel?] - Get results on popularity',
+        access: app.Config.accessLevels.identified,
+        call: popularityContest
+    });
+
+    // Get the total populairty of a user
+    const popularity = (to, from, text, message) => {
+        let [nick, channel] = text.split(' ');
+        // No nick available
+        if (!nick) {
+            app.say(to, `Could you possibly give me someone to rate?`);
+            return;
+        }
+
+        Models.Upvote.query(qb => {
+                qb.where('candidate', 'like', nick);
+                if (channel) {
+                    qb.andWhere('channel', 'like', channel);
+                }
+            })
+            .fetchAll()
+            .then(results => {
+                if (!results.length) {
+                    app.say(to, `There is no popularity data for ${nick}`);
+                    return;
+                }
+                let total = _(results.pluck('result')).sum();
+
+                app.say(to, `Popularity ${channel ? 'On ' + channel : ''} ${typo.icons.sideArrow} ${results.length} ${typo.icons.views} ${typo.icons.sideArrow} ${total} ${total > 0 ? typo.icons.happy : typo.icons.sad}`);
+            })
+            .catch(err => logger.err('Error fetching record', {
+                err
+            }));
+    };
+    app.Commands.set('popularity', {
+        desc: 'Get a users popularity',
+        access: app.Config.accessLevels.identified,
+        call: popularity
+    });
+
+
+    return scriptInfo;
 };
