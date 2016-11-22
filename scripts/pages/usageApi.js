8596a31fa2b22d67d19b86428a1645b85970aa98
Dave Richer <dave@develops.io>
2016-11-21 21:54:38 -0500

    updates

--- scripts/pages/usageApi.js
+++ scripts/pages/usageApi.js
@@ -5,13 +5,16 @@ const scriptInfo = {
     createdBy: 'Dave Richer'
 };
 
+const _ = require('lodash');
+const logger = require('../../lib/logger');
 const Models = require('bookshelf-model-loader');
 const Moment = require('moment');
-const _ = require('lodash');
 
 module.exports = app => {
     // No Database
     if (!Models.Logging) return;
+
+    const timeFormat = 'MMMM Do YYYY';
 
     // Get Usage Data over time
     const getUsageOverTime = (req, res) => {
@@ -21,24 +24,50 @@ module.exports = app => {
                     'timestamp'
                 ])
                 .count('to as messages')
-                .where('to','like',req.params.channel.replace('%23','#'))
+                .where('to', 'like', req.params.channel.replace('%23', '#'))
                 .groupBy([
-                  Models.Bookshelf.knex.raw('DATE(timestamp)'),
-                  'to'
+                    Models.Bookshelf.knex.raw('DATE(timestamp)'),
+                    'to'
                 ])
             )
             .fetchAll()
             .then(results => {
-                let final = _(results.toJSON()).map(value => {
+                if(!results.length) {
+                  res.json({
+                    message: 'No results available',
+                    status:'error',
+                    results:[],
+                  });
+                  return;
+                }
+                let computed = _(results.toJSON());
+                let final = computed.map(value => {
                     return {
                         channel: value.channel,
                         messages: value.messages,
-                        date: Moment(value.timestamp).format('MMM Do YY'),
+                        date: Moment(value.timestamp).format(timeFormat),
                     }
                 });
+
+                let lowest = computed.minBy('messages');
+                lowest.timestamp = Moment(lowest.timestamp).format(timeFormat);
+                let highest = computed.maxBy('messages');
+                highest.timestamp = Moment(highest.timestamp).format(timeFormat);
+
                 res.json({
-                  status: 'success',
-                  results: final
+                    status: 'success',
+                    results: final,
+                    lowest,
+                    highest,
+                });
+            })
+            .catch(err => {
+                logger.error('Error fetching usage stats', {
+                    err
+                });
+                res.json({
+                    status: 'error',
+                    results: []
                 });
             });
     };
