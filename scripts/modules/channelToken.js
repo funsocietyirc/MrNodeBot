11c43b3f1aa73e3ee583f383ace60f1a0ca32d05
Dave Richer <dave@develops.io>
2016-11-28 20:03:51 -0500

    - expose jobs in scheduler wrapper - add scheduled job to clean user tokens

--- scripts/modules/channelToken.js
+++ scripts/modules/channelToken.js
@@ -1,45 +1,70 @@
 'use strict';
 const scriptInfo = {
-  name: 'Channel Token',
-  desc: 'Give a IRC user a unique token that identifies them to a channel',
-  createdBy: 'Dave Richer'
+    name: 'Channel Token',
+    desc: 'Give a IRC user a unique token that identifies them to a channel',
+    createdBy: 'Dave Richer'
 };
 
+const moment = require('moment');
 const Models = require('bookshelf-model-loader');
+const logger = require('../../lib/logger');
 const randToken = require('rand-token');
-const tokenModel = Models.Token;
+const scheduler = require('../../lib/scheduler');
 
 module.exports = app => {
     // Log nick changes in the alias table
-    if (!app.Database && !Models.Token) {
-        return;
-    }
+    if (!app.Database && !Models.Token) return;
+
+    // Hold the token model
+    const tokenModel = Models.Token;
+
+    // Go through the users tokens and remove them if they are over a week old
+    const cleanTokens = scheduler.schedule('cleanTokens', {
+        hour: 0
+    }, () => {
+        let now = moment();
+
+        tokenModel
+            .fetchAll()
+            .then(results => results.forEach(result => {
+                // This is not a very old record, move on
+                if (now.diff(moment(result.attributes.timestamp), 'weeks') < 1) return;
+                return result
+                    .destroy()
+                    .then(deleted => logger.info(`User Token for ${result.attributes.user} on ${result.attributes.channel} has been removed`))
+                    .catch(err => logger.error('Error In removing a user token', {
+                        err
+                    }));
+            }))
+            .then(() => logger.info('Finished clearing User Token'))
+            .catch(err => logger.error('Error removing User tokens', {err}));
+    });
 
     // API End point, get a nick verified by channel token
     const getNickByTokenApi = (req, res) => {
-      let error = {
-          status: 'error',
-          result: null
-      };
-      let token = req.body.token;
-      if(!token) {
-        return res.json(error);
-      }
-      tokenModel.query(qb => {
-        qb
-        .where('token',token)
-        .select(['user','channel','timestamp']);
-      })
-      .fetch()
-      .then(result => {
-        if(!result) {
-          return res.json(error);
+        let error = {
+            status: 'error',
+            result: null
+        };
+        let token = req.body.token;
+        if (!token) {
+            return res.json(error);
         }
-        res.json({
-            status: 'success',
-            result: result
-        });
-      });
+        tokenModel.query(qb => {
+                qb
+                    .where('token', token)
+                    .select(['user', 'channel', 'timestamp']);
+            })
+            .fetch()
+            .then(result => {
+                if (!result) {
+                    return res.json(error);
+                }
+                res.json({
+                    status: 'success',
+                    result: result
+                });
+            });
     };
 
     // Register upload Handler
@@ -79,7 +104,7 @@ module.exports = app => {
                         })
                         .then(() => {
                             app.say(to, `Your token has been safely private messaged to you ${from}`);
-                            app.say(from, `Your new token for ${to} is ${token}`);
+                            app.say(from, `Your new token for ${to} is ${token}, it will expire in 7 days`);
                         });
                 }
                 // If previous token exists
