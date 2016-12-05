94a421047e185fbf96e19570a1a09f32a38e040e
Dave Richer <dave@develops.io>
2016-12-04 22:04:49 -0500

    use getShor lib in lmgtfy command

--- scripts/commands/lmgtfy.js
+++ scripts/commands/lmgtfy.js
@@ -5,7 +5,7 @@ const scriptInfo = {
     createdBy: 'IronY'
 };
 
-const gen = require('../generators/_isGdShortUrl');
+const gen = require('../lib/_getShortService');
 const logger = require('../../lib/logger');
 const ircTypography = require('../lib/_ircTypography');
 
