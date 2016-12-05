504a5c92da79adaf73e3ed1c4226436619dd426e
Dave Richer <dave@develops.io>
2016-12-05 03:49:01 -0500

    update last active

--- scripts/commands/lastActive.js
+++ scripts/commands/lastActive.js
@@ -5,6 +5,7 @@ const scriptInfo = {
     createdBy: 'IronY'
 };
 
+const _ = require('lodash');
 const Moment = require('moment');
 const Models = require('bookshelf-model-loader');
 
@@ -26,20 +27,69 @@ module.exports = app => {
             return;
         }
 
-        Models.Logging
-            .query(qb => qb
-                .select('to', 'from', 'text', 'timestamp')
-                .where('from', user)
-                .orderBy('timestamp', 'desc')
-                .limit(1)
-            )
-            .fetch()
-            .then(result => {
-                if (!result) {
-                    app.say(to, `${from}, I have never seen ${user} active`);
+        let logging = Models.Logging.query(qb => qb
+            .select('to', 'from', 'text', 'timestamp')
+            .where('from', user)
+            .orderBy('timestamp', 'desc')
+            .limit(1)).fetch().then(result => {
+            if (!result) return;
+            return new Object({
+                log: result.toJSON()
+            });
+        });
+        let partLogging = Models.PartLogging.query(qb => qb
+            .select('nick', 'channel', 'reason', 'timestamp')
+            .where('nick', user)
+            .orderBy('timestamp', 'desc')
+            .limit(1)).fetch().then(result => {
+            if (!result) return;
+            return new Object({
+                part: result.toJSON()
+            });
+        });
+        let quitLogging = Models.QuitLogging.query(qb => qb
+            .select('nick', 'channels', 'reason', 'timestamp')
+            .where('nick', user)
+            .orderBy('timestamp', 'desc')
+            .limit(1)).fetch().then(result => {
+            if (!result) return;
+            return new Object({
+                quit: result.toJSON()
+            });
+        });
+        let kickLogging = Models.KickLogging.query(qb => qb
+            .select('nick', 'channel', 'reason', 'timestamp')
+            .where('nick', user)
+            .orderBy('timestamp', 'desc')
+            .limit(1)).fetch().then(result => {
+            if (!result) return;
+            return new Object({
+                kick: result.toJSON()
+            });
+        });
+
+        Promise.all([logging, partLogging, quitLogging, kickLogging])
+            .then(results => {
+                // Clean result
+                results = _.compact(results);
+                // No Data available for user
+                if (!_.isArray(results) || _.isEmpty(results)) {
+                    app.say(to, `I have no data on ${user}`);
                     return;
                 }
-                app.say(to, `${result.get('from')} was last active ${Moment(result.get('timestamp')).fromNow()} on ${result.get('to')} saying: ${result.get('text')}`);
+                // Get the most recent result
+                results = _(results).sortBy(value => new Date(value.timestamp)).first();
+
+                // The last information we have was a post
+                if (results.log)
+                    app.say(to, `${results.log.from} was last active ${Moment(results.log.timestamp).fromNow()} on ${results.log.to} Saying: ${results.log.text}`);
+                else if (results.part)
+                    app.say(to, `${results.part.nick} was last active ${Moment(results.part.timestamp).fromNow()} on ${results.part.channel} Parting: ${results.part.reason}`);
+                else if (results.quit)
+                    app.say(to, `${results.quit.nick} was last active ${Moment(results.quit.timestamp).fromNow()} on ${results.kick.channels} Quitting: ${results.quit.reason}`);
+                else if (results.kick)
+                    app.say(to, `${results.kick.nick} was last active ${Moment(results.kick.timestamp).fromNow()} on ${results.kick.channel} Getting Kicked: ${results.kick.reason}`);
+
             });
     };
 
