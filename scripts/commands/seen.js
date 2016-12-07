5947ad9cef1b46b39a3b51866a61ac4685f3bfde
Dave Richer <dave@develops.io>
2016-12-07 06:57:38 -0500

    fix

--- scripts/commands/seen.js
+++ scripts/commands/seen.js
@@ -148,32 +148,39 @@ module.exports = app => {
 
                 // Hold the output
                 let output = new typo.StringBuilder();
-                output.appendBold(`Seen ${nick || user || host}`);
+                output.appendBold(`Seen`);
 
                 // Check the last thing said
                 let lastSaid = _(results).map('log').compact().first();
 
-                if (lastSaid)
-                    output.append(`saying`).append(`${lastSaid.text}`).append(`on ${lastSaid.to} ${Moment(lastSaid.timestamp).fromNow()}`);
+                if (lastSaid) output.append().append(`${lastSaid.from} saying`).append(`${lastSaid.text}`).append(`on ${lastSaid.to} ${Moment(lastSaid.timestamp).fromNow()}`);
 
                 // Get the most recent result
                 let lastResult = _(results).maxBy(value => Moment(value[Object.keys(value)[0]].timestamp).unix());
 
+
                 // Check other activity
-                if (lastResult.part)
+                if (lastResult.part) {
+                    if (!lastSaid) output.append(lastResult.part.nick);
                     output.append(`parting ${lastResult.part.channel} ${Moment(lastResult.part.timestamp).fromNow()}`).append(lastResult.part.reason);
-                else if (lastResult.quit)
+                } else if (lastResult.quit) {
+                    if (!lastSaid) output.append(lastResult.quit.nick);
                     output.append(`quitting [${lastResult.quit.channels}] ${Moment(lastResult.quit.timestamp).fromNow()}`).append(lastResult.quit.reason);
-                else if (lastResult.kick)
+                } else if (lastResult.kick) {
+                    if (!lastSaid) output.append(lastResult.kick.nick);
                     output.append(`getting kicked from ${lastResult.kick.channel} ${Moment(lastResult.kick.timestamp).fromNow()}`).append(lastResult.kick.reason);
-                else if (lastResult.join)
+                } else if (lastResult.join) {
+                    if (!lastSaid) output.append(lastResult.join.nick);
                     output.append(`joining ${lastResult.join.channel} ${Moment(lastResult.join.timestamp).fromNow()}`);
-                else if (lastResult.aliasOld) {
+                } else if (lastResult.aliasOld) {
+                    if (!lastSaid) output.append(lastResult.aliasold.oldnick);
                     output.append(`changing their nick to ${lastResult.aliasOld.newnick} in [${lastResult.aliasOld.channels}] ${Moment(lastResult.aliasOld.timestamp).fromNow()}`);
                     // Recurse on nick change
-                    seen(to, from, lastResult.aliasOld.newnick, message);
-                } else if (lastResult.aliasNew)
+                    //seen(to, from, lastResult.aliasOld.newnick, message);
+                } else if (lastResult.aliasNew) {
+                    if (!lastSaid) output.append(lastResult.aliasNew.newnick);
                     output.append(`changing their nick from ${lastResult.aliasNew.oldnick} in [${lastResult.aliasNew.channels}] ${Moment(lastResult.aliasNew.timestamp).fromNow()}`);
+                }
 
                 // For Some reason our output is empty
                 if (_.isEmpty(output.text)) {
