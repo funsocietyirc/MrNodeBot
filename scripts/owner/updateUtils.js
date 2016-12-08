62af731a35a8ccbd77edde13b9c502fb87d56079
Dave Richer <dave@develops.io>
2016-12-08 17:09:39 -0500

    fix

--- scripts/owner/updateUtils.js
+++ scripts/owner/updateUtils.js
@@ -71,7 +71,7 @@ module.exports = app => {
                 }
 
                 // No updates available
-                if(_.isString(stdOut) && stdOut === 'Already up-to-date.') {
+                if(_.isString(stdOut) && stdout === 'Already up-to-date.') {
                   app.say(to, 'I am still lemony fresh, no update required');
                   return;
                 }
