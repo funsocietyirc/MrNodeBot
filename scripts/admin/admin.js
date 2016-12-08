32663ff69a59c3f19871069daaedd9a269642f46
Dave Richer <dave@develops.io>
2016-12-08 18:55:32 -0500

    fix

--- scripts/admin/admin.js
+++ scripts/admin/admin.js
@@ -30,8 +30,7 @@ i18next.addResources('en', 'admin', {
 });
 
 /**
-  Administrator List manipulationr
-  Commands: admin [list add del help]
+  Administrator List manipulationr Commands: admin [list add del help]
 **/
 module.exports = app => {
     const admin = (to, from, text, message) => {
