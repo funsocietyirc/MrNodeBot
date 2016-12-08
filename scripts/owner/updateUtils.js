1e06ac9c1813038c8ddd0960fc215690ef04b9b2
Dave Richer <dave@develops.io>
2016-12-08 18:05:54 -0500

    fix

--- scripts/owner/updateUtils.js
+++ scripts/owner/updateUtils.js
@@ -116,16 +116,16 @@ module.exports = app => {
                     let shouldCycle = false;
                     let shouldNpm = false;
 
-                      // Iterate the results
-                      for (let file of diffFiles.split(os.EOL)) {
-                          console.dir(file);
-                          // Should we cycle
-                          if(!_.startsWith(file, 'scripts')) shouldCycle = true;
-                          // Should we update npm packages
-                          if(_.startsWith(file, 'package.json')) shouldNpm = true;
-                          // Exit early if we have what we need
-                          if (shouldCycle && shouldNpm) break;
-                      }
+                    // Iterate the results
+                    for (let file of diffFiles.split(os.EOL)) {
+                        console.dir(file);
+                        // Should we cycle
+                        if (!_.startsWith(file, 'scripts')) shouldCycle = true;
+                        // Should we update npm packages
+                        if (_.startsWith(file, 'package.json')) shouldNpm = true;
+                        // Exit early if we have what we need
+                        if (shouldCycle && shouldNpm) break;
+                    }
 
                     if (shouldNpm) {
                         app.say(to, 'Running NPM install..');
