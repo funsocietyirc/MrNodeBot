f20d9bc37444015e6dd67c2725125c63a7d5aa0d
Dave Richer <dave@develops.io>
2016-12-08 23:30:59 -0500

    test

--- scripts/owner/updateUtils.js
+++ scripts/owner/updateUtils.js
@@ -143,6 +143,7 @@ module.exports = app => {
                             }
                         })
                         .catch(err => {
+                            console.dir(err);
                             logger.error('Something went wrong in the update utils shorten chain', {
                                 err
                             });
