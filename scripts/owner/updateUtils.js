17410c19064510dff93bf08c58db7d6368d2bc0c
Dave Richer <dave@develops.io>
2016-12-08 19:18:45 -0500

    fix

--- scripts/owner/updateUtils.js
+++ scripts/owner/updateUtils.js
@@ -109,6 +109,7 @@ module.exports = app => {
                         .append(commit.authorDateRel)
                         .append(`${app.Config.project.repository.url}/commit/${commit.abbrevHash}`);
 
+
                         console.dir(output);
 
                     // Report we found an update
