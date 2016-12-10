a0c40784459061c2555090549db742545e4dfb22
Dave Richer <dave@develops.io>
2016-12-10 17:39:07 -0500

    add markov tests

---
+++ markovTest.js
@@ -0,0 +1,20 @@
+'use strict';
+const db = require('./database/client');
+const Models = require('bookshelf-model-loader');
+const config = require('./config');
+const markov = require('markov');
+
+const m = markov(2);
+
+Models.Logging.query(qb => qb.where('from', 'jeek')).fetchAll().then(results => {
+    let textArray = results.pluck('text');
+    textArray.forEach(text => m.seed(text.replace(/[\.\,]/g,' ')));
+    var stdin = process.openStdin();
+    console.log('> ');
+
+    stdin.on('data', function(line) {
+        var res = m.respond(line.toString()).join(' ');
+        console.log(res);
+        console.log('> ');
+    });
+});
