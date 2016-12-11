dc38e73e5eb17a1c807b9c4f2301bff455bd4e6b
Dave Richer <dave@develops.io>
2016-12-11 18:33:08 -0500

    This should not force a restart

--- scripts/listeners/popularityActionListener.js
+++ scripts/listeners/popularityActionListener.js
@@ -15,48 +15,48 @@ const scheduler = require('../../lib/scheduler');
 // Example IronY gives <nick> a plus or minus +1
 module.exports = app => {
     // Database not available
-    if(!Models.Upvote) return scriptInfo;
+    if (!Models.Upvote) return scriptInfo;
 
     const timeouts = new HashMap();
     const defaultPattern = /gives (.*) (\+|\-)1(?: (.*))?/;
 
     const pattern = (
-      _.isUndefined(app.Config.features.popularity) ||
-      _.isUndefined(app.Config.features.popularity.pattern) ||
-      !_.isString(app.Config.features.popularity.pattern)
+        _.isUndefined(app.Config.features.popularity) ||
+        _.isUndefined(app.Config.features.popularity.pattern) ||
+        !_.isString(app.Config.features.popularity.pattern)
     ) ? defaultPattern : app.Config.features.popularity.pattern;
 
     const delayMins = (
-      _.isUndefined(app.Config.features.popularity) ||
-      _.isUndefined(app.Config.features.popularity.delayInMins)
+        _.isUndefined(app.Config.features.popularity) ||
+        _.isUndefined(app.Config.features.popularity.delayInMins)
     ) ? 30 : app.Config.features.popularity.delayInMins;
 
     const delay = delayMins * 60 * 1000;
 
     const cleanJobInMins = (
-      _.isUndefined(app.Config.features.popularity) ||
-      _.isUndefined(app.Config.features.popularity.cleanJobInMins)
+        _.isUndefined(app.Config.features.popularity) ||
+        _.isUndefined(app.Config.features.popularity.cleanJobInMins)
     ) ? 30 : app.Config.features.popularity.cleanJobInMins;
 
     const ignoredChannels = (
-      _.isUndefined(app.Config.features.popularity) ||
-      _.isUndefined(app.Config.features.popularity.ignoredChannels) ||
-     !_.isArray(app.Config.features.popularity.ignoredChannels)
+        _.isUndefined(app.Config.features.popularity) ||
+        _.isUndefined(app.Config.features.popularity.ignoredChannels) ||
+        !_.isArray(app.Config.features.popularity.ignoredChannels)
     ) ? [] : app.Config.features.popularity.ignoredChannels;
 
     // Clean empty results periodically
     const clean = scheduler.schedule('clean-upvote-data', {
-      minute: cleanJobInMins
+        minute: cleanJobInMins
     }, () => {
         logger.info('Cleaning up upvote data');
-        timeouts.forEach((val,key) => {
-          if(_.isEmpty(val)) timeouts.remove(key);
+        timeouts.forEach((val, key) => {
+            if (_.isEmpty(val)) timeouts.remove(key);
         });
     });
 
     const popularity = (from, to, text, message) => {
         // Ignored channel
-        if(_.includes(ignoredChannels,to)) return;
+        if (_.includes(ignoredChannels, to)) return;
 
         // See if we get a match
         let result = text.match(pattern);
@@ -67,25 +67,25 @@ module.exports = app => {
         // Channels mismatch
 
         // Trying to vote on yourself
-        if(result[1] == from) {
-          app.say(from, `It is considered incredibly condescending to cast a vote for yourself`);
-          return;
+        if (result[1] == from) {
+            app.say(from, `It is considered incredibly condescending to cast a vote for yourself`);
+            return;
         }
 
         // Users are not in channel
-        if(!app._ircClient.isInChannel(to, result[1]) || !app._ircClient.isInChannel(to, from)) {
-          return;
+        if (!app._ircClient.isInChannel(to, result[1]) || !app._ircClient.isInChannel(to, from)) {
+            return;
         }
 
         // Timeout gate
-        if(timeouts.has(from)) {
-          let tmpTimeout = timeouts.get(from);
-          if(_.includes(tmpTimeout, result[1])) {
-            app.notice(from, `Your ${result[2]} vote for ${result[1]} has been rate limited (${delayMins} min total per candidate)`);
-            return;
-          };
+        if (timeouts.has(from)) {
+            let tmpTimeout = timeouts.get(from);
+            if (_.includes(tmpTimeout, result[1])) {
+                app.notice(from, `Your ${result[2]} vote for ${result[1]} has been rate limited (${delayMins} min total per candidate)`);
+                return;
+            };
         } else {
-          timeouts.set(from, []);
+            timeouts.set(from, []);
         }
 
         // Create the record
@@ -99,22 +99,21 @@ module.exports = app => {
                 user: message.user
             })
             .then(record => {
-              let tmpTimeout = timeouts.get(from);
-
-              if(!_.includes(tmpTimeout,result[1]))
-              {
-                tmpTimeout.push(result[1]);
-                timeouts.set(from, tmpTimeout);
-              }
-
-              // Set timeout
-              setTimeout(() => {
                 let tmpTimeout = timeouts.get(from);
-                _.pull(tmpTimeout, result[1]);
-                timeouts.set(from, tmpTimeout);
-              }, delay);
 
-              app.notice(from, `You have just given ${result[1]} a ${result[2]} vote on ${to}`)
+                if (!_.includes(tmpTimeout, result[1])) {
+                    tmpTimeout.push(result[1]);
+                    timeouts.set(from, tmpTimeout);
+                }
+
+                // Set timeout
+                setTimeout(() => {
+                    let tmpTimeout = timeouts.get(from);
+                    _.pull(tmpTimeout, result[1]);
+                    timeouts.set(from, tmpTimeout);
+                }, delay);
+
+                app.notice(from, `You have just given ${result[1]} a ${result[2]} vote on ${to}`)
             })
             .catch(err => logger.error(`Error in Popularity Listener`, {
                 err: err
