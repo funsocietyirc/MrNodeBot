3de2c14939dcd15d95671fa1015d46357e4047f6
Dave Richer <dave@develops.io>
2016-12-18 21:21:09 -0500

    bug fixes

--- scripts/beta/debugUtils.js
+++ scripts/beta/debugUtils.js
@@ -7,9 +7,26 @@ const scriptInfo = {
 };
 
 const _ = require('lodash');
+const util = require('util');
+const config = require('../../config');
 const logger = require('../../lib/logger.js');
+require('lodash-addons');
+
+const simpleString = object =>
+    util.inspect(object, {
+        depth: null
+    })
+    .replace(/<Buffer[ \w\.]+>/ig, '"buffer"')
+    .replace(/\[Function]/ig, 'function(){}')
+    .replace(/\[Circular]/ig, '"Circular"')
+    .replace(/\{ \[Function: ([\w]+)]/ig, '{ $1: function $1 () {},')
+    .replace(/\[Function: ([\w]+)]/ig, 'function $1(){}')
+    .replace(/(\w+): ([\w :]+GMT\+[\w \(\)]+),/ig, '$1: new Date("$2"),')
+    .replace(/(\S+): ,/ig, '$1: null,');
 
 module.exports = app => {
+    const lineLength = _.getNumber(_.get(config.irc.messageSplit), 512);
+
     // Evaluate
     app.Commands.set('eval', {
         desc: '[valid js] will return value to console',
@@ -17,10 +34,14 @@ module.exports = app => {
         call: (to, from, text, message) => {
             try {
                 let result = eval(text);
+                result = _.isString(result) ? result : simpleString(result);
+                let splitResults = result.split('\n') || [];
                 logger.info(`Eval result:`, {
-                    result: result
+                    result: splitResults
                 });
+                _.each(splitResults, line => app._ircClient.cSay(from, to, line));
             } catch (err) {
+                console.dir(err)
                 logger.error('Eval command failed:', {
                     err
                 });
