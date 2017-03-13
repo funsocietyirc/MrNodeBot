c03f078b3a0b9ce4bbf6d9d8e8527976a1f77b79
Dave Richer <dave@develops.io>
2017-03-13 15:10:08 -0400

    Refactors on gateway

--- scripts/pages/gateway.js
+++ scripts/pages/gateway.js
@@ -7,42 +7,50 @@ const scriptInfo = {
 };
 
 module.exports = app => {
-    // Landing Page
+
+    // Notification Gateway
     app.WebRoutes.set('gateway', {
         handler: (req, res) => {
-            // No Userinfo
-            if (!req.userInfo)
-                return res.json({
-                    success: false,
-                    message: 'No userInfo was provided'
-                });
-            // No Payload / recipient
-            if (!req.body.payload || !req.body.recipient)
-                return res.json({
-                    success: false,
-                    message: 'Missing payload or recipient'
-                });
-
             // An Admin account is needed
-            if(!req.userInfo.admin) {
-              return res.json({
+            if (!req.userInfo || !req.userInfo.admin) return res.json({
                 success: false,
-                message: 'You must be an Administrator to use this gateway'
-              })
-            }
+                message: 'Not enough information to complete this request'
+            });
+
+            // No Payload / recipient
+            if (!req.body.payload || !req.body.recipient) return res.json({
+                success: false,
+                message: 'Missing payload or recipient'
+            });
 
             // A Very basic message
             app.say(req.body.recipient, req.body.payload);
 
             // Send back all is ok
             return res.json({
-              success: true,
-              message: `Message delivery to ${req.body.recipient} attempted`
+                success: true,
+                message: `Message delivery to ${req.body.recipient} attempted`
             })
         },
         desc: 'Gateway',
         path: '/gateway',
         name: 'landingPage',
+        verb: 'post',
+        secure: true
+    });
+
+    // General Logging Gateway
+    app.WebRoutes.set('discordLogging', {
+        handler: (req, res) => {
+            // An Admin account is needed
+            if (!req.userInfo) return res.json({
+                success: false,
+                message: 'Not enough information to complete this request'
+            });
+
+        },
+        desc: 'Discord Logging',
+        path: 'discordLogging',
         verb: 'post',
         secure: true
     });
