3810ccb416e6d4570d74edc4577d255ca1787ddc
Dave Richer <dave@develops.io>
2017-04-14 00:17:14 -0400

    youtube channel broadcast

--- scripts/urlListener/_getYoutube.js
+++ scripts/urlListener/_getYoutube.js
@@ -3,7 +3,6 @@ const _ = require('lodash');
 const gen = require('../generators/_youTubeVideoData');
 const apiKey = require('../../config').apiKeys.google;
 const logger = require('../../lib/logger');
-
 const pusherApi = require('../../lib/pusher');
 
 
@@ -20,24 +19,26 @@ module.exports = (key, results) => new Promise(resolve => {
             if (!data) return resolve(results);
             // Set youtube data on results object
             results.youTube = {
+                key: key,
                 videoTitle: data.snippet.title,
                 viewCount: numberOrNa(data.statistics.viewCount),
                 likeCount: numberOrNa(data.statistics.likeCount),
                 dislikeCount: numberOrNa(data.statistics.dislikeCount),
                 commentCount: numberOrNa(data.statistics.commentCount)
             };
-
             // Fire off youtube data
             if (pusherApi) {
-                let pusherVars = Object.assign({
-                        to,
-                        from,
-                        timestamp,
-                        key,
-                        url: results.url
-                    },
-                    results.youTube
-                );
+              console.dir(psuherApi);
+                let pusherVars = {
+                    to: results.to,
+                    from: results.from,
+                    timestamp: results.timestamp,
+                    videoTitle: results.youTube.videoTitle,
+                    youtubeKey: results.youTube.key,
+                    url: results.url
+                };
+                console.dir(pusherVars);
+
                 pusherApi.trigger('public', 'youtube', pusherVars);
             }
 
