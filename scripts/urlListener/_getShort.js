1076ad8460af4f3696b8d175e8ee1a4d22380e58
Dave Richer <dave@develops.io>
2017-05-05 13:33:10 -0400

    async / travis modification to only support node 7

--- scripts/urlListener/_getShort.js
+++ scripts/urlListener/_getShort.js
@@ -3,16 +3,12 @@ const _ = require('lodash');
 const short = require('../lib/_getShortService');
 const logger = require('../../lib/logger');
 
-module.exports = async results => {
+module.exports = async(results) => {
   try {
     let shortUrl = await short(results.url);
-    Object.assign(results, {
-      shortUrl: shortUrl
-    });
+    Object.assign(results, {shortUrl: shortUrl});
   } catch (err) {
-    logger.warn('Error in URL Shortner function', {
-      err
-    });
+    logger.warn('Error in URL Shortner function', {err});
   } finally {
     return results;
   }
