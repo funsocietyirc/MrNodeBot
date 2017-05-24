b79ab9421953cb7cd42eda7a534393cdc6a4d79f
Dave Richer <dave@develops.io>
2017-05-24 13:09:30 -0400

    enhanceements

--- scripts/generators/_ipLocationData.js
+++ scripts/generators/_ipLocationData.js
@@ -4,22 +4,6 @@ const rp = require('request-promise-native');
 const logger = require('../../lib/logger');
 
 const endPoint = 'https://freegeoip.net/json/';
-// Return GEO IP Data in the following format
-//     {
-//     ip: '',
-//     country_code: '',
-//     country_name: '',
-//     region_code: '',
-//     region_name: '',
-//     city: '',
-//     zip_code: '',
-//     time_zone: '',
-//     latitude: 0,
-//     longitude: 0,
-//     metro_code: 0
-//   }
-//
-// If the host is invalid, and the request fails, a empty object will be returned
 
 module.exports = async (host) => {
     if (!host)
@@ -42,3 +26,20 @@ module.exports = async (host) => {
         return {};
     }
 };
+
+// Return GEO IP Data in the following format
+//     {
+//     ip: '',
+//     country_code: '',
+//     country_name: '',
+//     region_code: '',
+//     region_name: '',
+//     city: '',
+//     zip_code: '',
+//     time_zone: '',
+//     latitude: 0,
+//     longitude: 0,
+//     metro_code: 0
+//   }
+//
+// If the host is invalid, and the request fails, a empty object will be returned
