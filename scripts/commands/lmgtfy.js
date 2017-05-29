a896b810ba67cf21c44699ba7a9bb9dbd10023ae
Dave Richer <dave@develops.io>
2017-05-28 21:34:37 -0400

    - async / await lmgtfy

--- scripts/commands/lmgtfy.js
+++ scripts/commands/lmgtfy.js
@@ -9,24 +9,32 @@ const logger = require('../../lib/logger');
 const ircTypography = require('../lib/_ircTypography');
 
 module.exports = app => {
+    const lmgtfy = async (to, from, text, message) => {
+        if (!text) {
+            app.say(to, 'You need to give me some more information...');
+            return;
+        }
+        try {
+            // Grab result
+            const result = await gen('http://lmgtfy.com/?q=' + encodeURIComponent(text));
+            // Report back
+            app.say(to, !result ?
+                'Something went wrong figuring that out for you' :
+                `${ircTypography.logos.lmgtfy} ${result}`)
+        }
+        catch (err) {
+            logger.error(`Something went wrong in the LMGTFY command`, {
+                message: err.message || '',
+                stack: err.stack || '',
+            });
+            app.say(to, `Something went wrong fetching LMGTFY data for you ${from}`);
+        }
+    };
     app.Commands.set('lmgtfy', {
         desc: '[search text] - Figure something out for someone',
         access: app.Config.accessLevels.identified,
-        call: (to, from, text, message) => {
-            if (!text) {
-                app.say(to, 'You need to give me some more information...');
-                return;
-            }
-            gen('http://lmgtfy.com/?q=' + encodeURIComponent(text))
-                .then(result =>
-                    app.say(to, !result ?
-                        'Something went wrong figuring that out for you' :
-                        `${ircTypography.logos.lmgtfy} ${result}`)
-                )
-                .catch(err => logger.error('LMGTFY Error', {
-                    err
-                }));
-        }
+        call: lmgtfy
     });
+
     return scriptInfo;
 };
