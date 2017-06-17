c4fe42a43cb6c402764689223e35f271c6d7be79
Dave Richer <dave@develops.io>
2017-06-17 02:08:19 -0400

    - finalize randomSentiment - async refactors on FML line

--- scripts/commands/fml.js
+++ scripts/commands/fml.js
@@ -10,27 +10,35 @@ const logger = require('../../lib/logger');
 const ircTypography = require('../lib/_ircTypography');
 
 module.exports = app => {
+
+    const fmlLine = async (to, from, text, message) => {
+        try {
+            const result = await gen();
+            if (!result) {
+                app.say(to, 'I could not seem to find any FML lines');
+                return;
+            }
+            let output = new ircTypography.StringBuilder({
+                logo: 'fml'
+            });
+            output.append(result[0]);
+            app.say(to, output.text);
+
+        }
+        catch (err) {
+            logger.error('FML Command Error:', {
+                message: err.message || '',
+                stack: err.stack || '',
+            });
+            app.say(to, 'Something went wrong with the FML API');
+        }
+    };
+
     app.Commands.set('fml', {
         desc: 'Get a random FML quote',
         access: app.Config.accessLevels.identified,
-        call: (to, from, text, message) => gen()
-            .then(result => {
-                if (!result) {
-                    app.say(to, 'I could not seem to find any FML lines');
-                    return;
-                }
-                let output = new ircTypography.StringBuilder({
-                    logo: 'fml'
-                });
-                output.append(result[0]);
-                app.say(to, output.text);
-            })
-            .catch(err => {
-                app.say(to, 'Something went wrong with the FML API');
-                logger.error('FML Command Error:', {
-                    err
-                });
-            })
+        call: fmlLine
     });
     return scriptInfo;
 };
+
--- scripts/commands/randomSentiment.js
+++ scripts/commands/randomSentiment.js
@@ -16,38 +16,46 @@ module.exports = async (app) => {
     // Database not available
     if (!Models.Upvote) return scriptInfo;
 
+    // Get a random sentiment
+    const randomSentiment = async (to, from, text, message) => {
+        try {
+            // Grab results
+            const result = await Models.Upvote.query(
+                qb => qb
+                    .select('candidate', 'voter','text','result','channel','timestamp')
+                    .where('channel', to)
+                    .whereNotNull('text')
+                    .orderByRaw('rand()')
+                    .limit(1)
+            ).fetch();
+
+            // No results
+            if(!result) {
+                app.say(to,`It seems there is no sentiments for ${to}, ${from}`);
+                return;
+            }
+
+            // Format Disposition
+            const disposition = result.get('result') === 1 ? '{likes|adores|commends}' : '{dislikes|hates|despises}';
+
+            // Report back
+            app.say(to, `${result.get('voter')} ${disposition} ${result.get('candidate')} ${result.get('text')} [${Moment(result.get('timestamp')).fromNow()}]`);
+        }
+        catch (err) {
+            logger.error('Something went wrong in randomSentiment', {
+                message: err.message || '',
+                stack: err.stack || ''
+            });
+            app.say(to, `Something went wrong finding a random sentiment, ${from}`);
+        }
+    };
+
     // random-sentiment command
     app.Commands.set('random-sentiment', {
         desc: 'Get a random sentiment',
         access: app.Config.accessLevels.identified,
-        call: async (to, from, text, message) => {
-            try {
-                const result = await Models.Upvote.query(
-                    qb => qb
-                        .select('candidate', 'voter','text','result','channel','timestamp')
-                        .where('channel', to)
-                        .whereNotNull('text')
-                        .orderByRaw('rand()')
-                        .limit(1)
-                ).fetch();
-
-                if(!result) {
-                    app.say(to,`It seems there is no sentiments for ${to}, ${from}`);
-                    return;
-                }
-
-                const disposition = result.get('result') === 1 ? '{likes|adores}' : '{dislikes|hates}';
-                let output = `${result.get('voter')} ${disposition} ${result.get('candidate')} ${result.get('text')} [${Moment(result.get('timestamp')).fromNow()}]`;
-                app.say(to, output);
-            }
-            catch (err) {
-                logger.error('Something went wrong in randomSentiment', {
-                    message: err.message || '',
-                    stack: err.stack || ''
-                });
-                app.say(to, `Something went wrong finding a random sentiment, ${from}`);
-            }
-        }
+        call: randomSentiment
     });
 
+    return scriptInfo;
 };
