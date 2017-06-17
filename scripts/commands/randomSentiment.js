ef80588363df505e8401211131203aca8b62c6a6
Dave Richer <dave@develops.io>
2017-06-17 01:51:31 -0400

    - error catching on random-sentiment

--- scripts/commands/randomSentiment.js
+++ scripts/commands/randomSentiment.js
@@ -20,26 +20,33 @@ module.exports = async (app) => {
         desc: 'Get a random sentiment',
         access: app.Config.accessLevels.identified,
         call: async (to, from, text, message) => {
+            try {
+                const result = await Models.Upvote.query(
+                    qb => qb
+                        .select('candidate', 'voter','text','result','channel')
+                        .where('channel', to)
+                        .whereNotNull('text')
+                        .orderByRaw('rand()')
+                        .limit(1)
+                ).fetch();
 
+                if(!result) {
+                    app.say(to,`It seems there is no sentiments for ${to}, ${from}`);
+                    return;
+                }
 
-            const result = await Models.Upvote.query(
-                qb => qb
-                    .select('candidate', 'voter','text','result','channel')
-                    .where('channel', to)
-                    .whereNotNull('text')
-                    .orderByRaw('rand()')
-                    .limit(1)
-            ).fetch();
-
-            if(!result) {
-                app.say(to,`It seems there is no sentiments for ${to}, ${from}`);
-                return;
+                const disposition = result.attributes.result === 1 ? '{likes|adores}' : '{dislikes|hates}';
+                let output = `${result.attributes.voter} ${disposition} ${result.attributes.candidate}`;
+                if(result.attributes.text) output = `${output} ${result.attributes.text}`;
+                app.say(to, output);
             }
-
-            const disposition = result.attributes.result === 1 ? '{likes|adores}' : '{dislikes|hates}';
-            let output = `${result.attributes.voter} ${disposition} ${result.attributes.candidate}`;
-            if(result.attributes.text) output = `${output} ${result.attributes.text}`;
-            app.say(to, output);
+            catch (err) {
+                logger.error('Something went wrong in randomSentiment', {
+                    message: err.message || '',
+                    stack: err.stack || ''
+                });
+                app.say(to, `Something went wrong finding a random sentiment, ${from}`);
+            }
         }
     });
 
