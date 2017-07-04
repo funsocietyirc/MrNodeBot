a59da75e7c744acac31887a1769ba5a34558dbfc
Dave Richer <dave@develops.io>
2017-07-03 20:38:39 -0400

    - async refactors on _getYoutube and _linkMatcher

--- scripts/urlListener/_getYoutube.js
+++ scripts/urlListener/_getYoutube.js
@@ -5,67 +5,69 @@ const gen = require('../generators/_youTubeVideoData');
 const apiKey = require('../../config').apiKeys.google;
 const logger = require('../../lib/logger');
 
-module.exports = (key, list, index, seekTime, results) => new Promise(resolve => {
+const getYoutube = async(key, list, index, seekTime, results) => {
     // No Key provided, return the results
     if (
         (!_.isString(key) || _.isEmpty(key)) &&
         (!_.isString(list) || _.isEmpty(list))
-    ) return resolve(results);
+    ) return results;
 
     // Normalize Helper
     const numberOrZero = number => !isNaN(number) ? number : 0;
 
-    return gen(apiKey, key, list)
-        .then(result => {
-            // We have no data, default back to the original title grabber
-            if (!result) return resolve(results);
-            // Initialize youtube results
-            results.youTube = {};
+    try {
+        const result = await gen(apiKey, key, list);
 
-            // Current Video Index
-            if (!isNaN(index)) results.youTube.index = index;
-            // Video Seek Time
-            if (!isNaN(seekTime)) results.youTube.seekTime = seekTime;
+        // We have no data, default back to the original title grabber
+        if (!result) return results;
+        // Initialize youtube results
+        results.youTube = {};
 
-            // We have Video Results
-            if (!_.isEmpty(result.videoResults)) {
-                // Hold on to the first element, which should be our selection since we are searching by key
-                const videoResults = result.videoResults[0];
+        // Current Video Index
+        if (!isNaN(index)) results.youTube.index = index;
+        // Video Seek Time
+        if (!isNaN(seekTime)) results.youTube.seekTime = seekTime;
 
-                results.youTube.video = {
-                    key: key,
-                    videoTitle: videoResults.snippet.title || '',
-                    viewCount: numberOrZero(videoResults.statistics.viewCount),
-                    likeCount: numberOrZero(videoResults.statistics.likeCount),
-                    dislikeCount: numberOrZero(videoResults.statistics.dislikeCount),
-                    commentCount: numberOrZero(videoResults.statistics.commentCount),
-                    channelTitle: videoResults.snippet.channelTitle || '',
-                    restrictions: _.isObject(videoResults.contentDetails.regionRestriction),
-                    embeddable: _.isObject(videoResults.status) && videoResults.status.embeddable
-                };
-            }
+        // We have Video Results
+        if (!_.isEmpty(result.videoResults)) {
+            // Hold on to the first element, which should be our selection since we are searching by key
+            const videoResults = result.videoResults[0];
 
-            // We have Playlist Results
-            if (!_.isEmpty(result.playlistResults)) {
-                // Hold on to the first playlist, the result should be expected as we are searching by key
-                const playlistResults = result.playlistResults[0];
+            results.youTube.video = {
+                key: key,
+                videoTitle: videoResults.snippet.title || '',
+                viewCount: numberOrZero(videoResults.statistics.viewCount),
+                likeCount: numberOrZero(videoResults.statistics.likeCount),
+                dislikeCount: numberOrZero(videoResults.statistics.dislikeCount),
+                commentCount: numberOrZero(videoResults.statistics.commentCount),
+                channelTitle: videoResults.snippet.channelTitle || '',
+                restrictions: _.isObject(videoResults.contentDetails.regionRestriction),
+                embeddable: _.isObject(videoResults.status) && videoResults.status.embeddable
+            };
+        }
 
-                results.youTube.playlist = {
-                    key: list,
-                    videoCount: numberOrZero(playlistResults.contentDetails.itemCount),
-                    playlistTitle: playlistResults.snippet.title || '',
-                };
+        // We have Playlist Results
+        if (!_.isEmpty(result.playlistResults)) {
+            // Hold on to the first playlist, the result should be expected as we are searching by key
+            const playlistResults = result.playlistResults[0];
 
-            }
+            results.youTube.playlist = {
+                key: list,
+                videoCount: numberOrZero(playlistResults.contentDetails.itemCount),
+                playlistTitle: playlistResults.snippet.title || '',
+            };
+        }
 
-            // Return results
-            resolve(results);
-        })
-        .catch(err => {
-            logger.warn('Error in YouTube link function', {
-                err: err.stack,
-            });
-            resolve(results);
+        // Return results
+        return results;
+    }
+    catch (err) {
+        logger.warn('Error in YouTube link function', {
+            message: err.message || '',
+            stack: err.stack,
         });
+       return results;
+    }
+};
 
-});
+module.exports = getYoutube;
--- scripts/urlListener/_linkMatcher.js
+++ scripts/urlListener/_linkMatcher.js
@@ -1,40 +1,36 @@
 'use strict';
 
-const config = require('../../config');
-
+const _ = require('lodash');
 const URL = require('url').URL; // TODO Here until we can figure why URI is not parsing the whole query string
 
-const _ = require('lodash');
+const config = require('../../config');
 const getYoutube = require('./_getYoutube.js'); // Get the youtube key from link
 const getImdb = require('./_getImdb.js'); // Get IMDB Data
 const getGitHub = require('./_getGithub'); // Get GitHub Information
 const getBitBucket = require('./_getBitBucket'); // Get BitBucket Information
 const getImgur = require('./_getImgurImage'); // Get Imgur data
 
-
-module.exports = results => new Promise(resolve => {
+const linkMatcher = async (results) => {
     // Use the realUrl if available when doing matches
     // This allows shortened urls to still hit
     const current = results.realUrl ? results.realUrl : results.url;
     const url = new URL(current); // TODO Node URL Module because the URI model has some weird query string parsing issues
 
     // No URI or URL
-    if (!results.uri || !url) return resolve(results);
+    if (!results.uri || !url) return results;
 
     switch (url.hostname) {
         case 'youtu.be':
         case 'www.youtu.be':
             const id = results.uri.segmentCoded(0);
-            if(_.isString(id) && !_.isEmpty(id)) {
-              return resolve(
-                  getYoutube(
-                      id,
-                      url.searchParams.get('list'),
-                      url.searchParams.get('index'),
-                      url.searchParams.get('t'),
-                      results
-                  )
-              );
+            if (_.isString(id) && !_.isEmpty(id)) {
+                return getYoutube(
+                    id,
+                    url.searchParams.get('list'),
+                    url.searchParams.get('index'),
+                    url.searchParams.get('t'),
+                    results
+                )
             }
             break;
         case 'youtube.com': // Youtube
@@ -43,14 +39,12 @@ module.exports = results => new Promise(resolve => {
                 // Play list link
                 case 'playlist':
                     if (_.isString(url.searchParams.get('list')))
-                        return resolve(
-                            getYoutube(
-                                null,
-                                url.searchParams.get('list'),
-                                url.searchParams.get('index'),
-                                url.searchParams.get('t'),
-                                results
-                            )
+                        return getYoutube(
+                            null,
+                            url.searchParams.get('list'),
+                            url.searchParams.get('index'),
+                            url.searchParams.get('t'),
+                            results
                         );
                     break;
                 case 'embed':
@@ -58,25 +52,21 @@ module.exports = results => new Promise(resolve => {
                 default:
                     // Playlist
                     if (_.isString(url.searchParams.get('list')) && _.isString(url.searchParams.get('v')))
-                        return resolve(
-                            getYoutube(
-                                url.searchParams.get('v'),
-                                url.searchParams.get('list'),
-                                url.searchParams.get('index'),
-                                url.searchParams.get('t'),
-                                results
-                            )
+                        return getYoutube(
+                            url.searchParams.get('v'),
+                            url.searchParams.get('list'),
+                            url.searchParams.get('index'),
+                            url.searchParams.get('t'),
+                            results
                         );
                     // Single Video
                     else if (_.isString(url.searchParams.get('v')))
-                        return resolve(
-                            getYoutube(
-                                url.searchParams.get('v'),
-                                null,
-                                url.searchParams.get('index'),
-                                url.searchParams.get('t'),
-                                results
-                            )
+                        return getYoutube(
+                            url.searchParams.get('v'),
+                            null,
+                            url.searchParams.get('index'),
+                            url.searchParams.get('t'),
+                            results
                         );
                     break;
 
@@ -85,11 +75,11 @@ module.exports = results => new Promise(resolve => {
         case 'imdb.com': // IMDB
         case 'www.imdb.com':
             // No API key for OMDB Provided
-            if(!_.isString(config.apiKeys.omdb) || _.isEmpty(config.apiKeys.omdb)) break;
+            if (!_.isString(config.apiKeys.omdb) || _.isEmpty(config.apiKeys.omdb)) break;
             let segments = results.uri.segmentCoded();
             if (segments.indexOf('title') !== -1) {
                 let titleId = results.uri.segmentCoded(segments.indexOf('title') + 1);
-                if ( _.isString(titleId) && titleId.startsWith('tt')) return resolve(getImdb(titleId, results));
+                if (_.isString(titleId) && titleId.startsWith('tt')) return getImdb(titleId, results);
             }
             break;
         case 'imgur.com': // Imgur
@@ -99,32 +89,33 @@ module.exports = results => new Promise(resolve => {
                 if (!segment) break;
                 let imageId = segment.substr(0, segment.lastIndexOf('.'));
                 if (!imageId) break;
-                if (imageId) return resolve(getImgur('image', imageId, results));
+                if (imageId) return getImgur('image', imageId, results);
             }
             switch (results.uri.segmentCoded(0)) {
                 case 'image':
                 case 'gallery':
-                    if (results.uri.segmentCoded(1)) return resolve(getImgur(results.uri.segmentCoded(0), results.uri.segmentCoded(1), results));
+                    if (results.uri.segmentCoded(1)) return getImgur(results.uri.segmentCoded(0), results.uri.segmentCoded(1), results);
                     break;
                 case 'album':
                 case 'a':
-                    if (results.uri.segmentCoded(1)) return resolve(getImgur('album', results.uri.segmentCoded(1), results));
+                    if (results.uri.segmentCoded(1)) return getImgur('album', results.uri.segmentCoded(1), results);
                     break;
                 default:
-                    if (results.uri.segment().length === 1) return resolve(getImgur('image', results.uri.segmentCoded(0), results));
+                    if (results.uri.segment().length === 1) return getImgur('image', results.uri.segmentCoded(0), results);
                     break;
             }
             break;
         case 'github.com': // GitHub
         case 'www.github.com':
-            if (results.uri.segment().length >= 2) return resolve(getGitHub(results.uri.segmentCoded(0), results.uri.segmentCoded(1), results)); // 2: User, 3: Repo
+            if (results.uri.segment().length >= 2) return getGitHub(results.uri.segmentCoded(0), results.uri.segmentCoded(1), results); // 2: User, 3: Repo
             break;
         case 'bitbucket.org': // BitBucket
         case 'www.butbucket.com':
-            if (results.uri.segment().length >= 2) return resolve(getBitBucket(results.uri.segmentCoded(0), results.uri.segmentCoded(1), results)); // 2: User, 3: Repo
+            if (results.uri.segment().length >= 2) return getBitBucket(results.uri.segmentCoded(0), results.uri.segmentCoded(1), results); // 2: User, 3: Repo
             break;
     }
 
-    // No Matches
-    return resolve(results);
-});
+    return results;
+};
+
+module.exports = linkMatcher;
