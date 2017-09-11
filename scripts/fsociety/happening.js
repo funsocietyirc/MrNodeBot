2a47ac5f03a1f67c9b3a42834a2a2fe80837ea6a
Dave Richer <dave@develops.io>
2017-09-10 23:28:49 -0400

    extend countdown functionality

--- scripts/fsociety/happening.js
+++ scripts/fsociety/happening.js
@@ -1,7 +1,7 @@
 'use strict';
 const scriptInfo = {
     name: 'Happening',
-    desc: 'MrRobot Season 3 countdown',
+    desc: 'Channel countdowns',
     createdBy: 'IronY'
 };
 const _ = require('lodash');
@@ -11,31 +11,76 @@ const scheduler = require("../../lib/scheduler.js");
 // Extend moment with countdown
 require('moment-countdown');
 
+const getCountdown = (when) => moment(when).countdown();
+
 module.exports = app => {
-    const showTime = new Date(2017, 9, 11, 22, 0, 0, 0);
+    // No Configuration available, bail
+    if (!_.isArray(app.Config.features.countdowns)) return scriptInfo;
 
-    // Season 3 Countdown
-    const getCountdown = () => moment(showTime).countdown();
+    // Hold channel message call backs
+    const channelMessages = new Map();
 
-    // Countdown text options
-    const textOptions = '{is Happening in|is Coming to a screen near you in|is Hacking all the things in|will be brought to you in|drops in|is hacking your democracy in}';
+    // Format Countdown
+    const getCountdown = (when) => moment(when).countdown();
 
-    // Season 3 Countdown Message
-    const getCountdownMessage = () => `Season 3 of Mr. Robot ${textOptions} ${getCountdown().toString()}!!`;
+    // Process countdown objects
+    const processCountdowns = (countdowns) => {
+        if (!_.isArray(countdowns)) return;
 
-    // Scheduled every night
-    let cronTime = new scheduler.RecurrenceRule();
-    cronTime.second = 0;
-    cronTime.minute = 0;
-    cronTime.hour = 0;
-    scheduler.schedule('timeUntilMrRobot', cronTime, () => app.say('#fsociety', getCountdownMessage()));
+        // Iterate over countdowns
+        for (const countdown of countdowns) {
+
+            // Invalid object, bail
+            if (
+                !_.isObject(countdown) ||
+                !_.isString(countdown.who) ||
+                !countdown.hasOwnProperty('when') ||
+                !_.isArray(countdown.what) ||
+                !_.isString(countdown.where) ||
+                !_.isObject(countdown.why)
+            ) continue;
+
+            // Build announcement message
+            const getCountdownMessage = () => `${countdown.who} ${_.sample(countdown.what)} ${getCountdown(countdown.when).toString()} on ${countdown.where}`;
+
+            // Irc block present
+            if (countdown.why.hasOwnProperty('irc') && _.isObject(countdown.why.irc)) _.each(countdown.why.irc, (v, k) => {
+
+                // Assign Countdown message
+                channelMessages.set(k, getCountdownMessage);
+
+                // Bind announcements
+                if (_.isArray(v.announcements)) for (const announcement of v.announcements) {
+                    scheduler.schedule(countdown.who + k, new scheduler.RecurrenceRule(announcement.year, announcement.month, announcement.date, announcement.dayOfWeek, announcement.hour, announcement.minute, announcement.second), () => {
+                        // Not in channel
+                        if (!app._ircClient.isInChannel(k)) return;
+                        // Announce
+                        app.say(k, getCountdownMessage());
+                    });
+                }
+
+            });
+        }
+    };
+
+    // Handle the IRC Command
+    const happening = (to, from, text, message) => {
+        if (!channelMessages.has(to)) {
+            app.say(to, `There are currently no countdowns available for ${to}, ${from}`);
+            return;
+        }
+        app.say(to, channelMessages.get(to)());
+    };
 
     // IRC Command
     app.Commands.set('happening', {
         desc: 'Mr. Robot Season 3 countdown',
-        access: app.Config.accessLevels.admin,
-        call: (to, from, text, message) => app.say(to, getCountdownMessage())
+        access: app.Config.accessLevels.identified,
+        call: happening
     });
 
-    return scriptInfo;
+    // Return the script info
+    return Object.assign({}, scriptInfo, {
+        onLoad: processCountdowns(app.Config.features.countdowns),
+    });
 };
