b3d2cfe2e276232029f4f277b54187af2a74c1d3
Dave Richer <dave@ir0ny.com>
2018-02-04 00:24:41 -0500

    optimize

--- scripts/lib/_extraThicc.js
+++ scripts/lib/_extraThicc.js
@@ -6,7 +6,9 @@ const intraword2 = [
     '丫', '乙', '卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁', '长', '乚', '从', '𠘨', '口', '尸', '㔿', '尺', '丂', '丅', '凵', 'リ', '山', '乂', '丫', '乙', '  ',
 ];
 module.exports = (text) => {
+    let textArr = text.split('');
     for (let x = 0; x < intraword1.length - 1; x++) {
+        if (!textArr.includes(intraword1[x])) continue;
         text = text.replace(new RegExp(intraword1[x], 'g'), intraword2[x]);
     }
     return text;
