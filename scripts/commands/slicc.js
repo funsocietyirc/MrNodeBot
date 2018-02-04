649c292b4c79c8b9b8bf7eaa562394d5774dd1b5
Dave Richer <dave@ir0ny.com>
2018-02-04 00:14:36 -0500

    add the slicc command

---
+++ scripts/commands/slicc.js
@@ -0,0 +1,23 @@
+const slicc = require('../lib/_extraThicc');
+
+const scriptInfo = {
+    name: 'slicc',
+    desc: 'Slicc text back to the IRC user, a simple test script',
+    createdBy: 'IronY',
+};
+
+module.exports = (app) => {
+    // Echo Test command
+    app.Commands.set('slicc', {
+        desc: '[text] Exactly what it sounds like',
+        access: app.Config.accessLevels.admin,
+        call: (to, from, text, message) => {
+            const newText = slicc(text, 'backward');
+            console.dir(newText);
+            app.say(to, newText);
+        }
+    });
+
+    // Return the script info
+    return scriptInfo;
+};
