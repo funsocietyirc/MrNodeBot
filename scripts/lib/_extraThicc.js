8794c326e3d7b7908702b89afc85f8f8517df549
Dave Richer <dave@ir0ny.com>
2018-02-04 01:18:53 -0500

    remove extra case

--- scripts/lib/_extraThicc.js
+++ scripts/lib/_extraThicc.js
@@ -1,15 +1,14 @@
 const intraword1 = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '
+];
 const intraword2 = [
-    '卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁', '长', '乚', '从', '𠘨', '口', '尸', '㔿', '尺', '丂', '丅', '凵', 'リ', '山', '乂',
-    '丫', '乙', '卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁', '长', '乚', '从', '𠘨', '口', '尸', '㔿', '尺', '丂', '丅', '凵', 'リ', '山', '乂', '丫', '乙', '  ',
+    '卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁', '长', '乚', '从', '𠘨', '口', '尸', '㔿', '尺', '丂', '丅', '凵', 'リ', '山', '乂', '  ',
 ];
 module.exports = (text) => {
     let textArr = text.split('');
     for (let x = 0; x < intraword1.length - 1; x++) {
         if (!textArr.includes(intraword1[x])) continue;
-        text = text.replace(new RegExp(intraword1[x], 'g'), intraword2[x]);
+        text = text.replace(new RegExp(intraword1[x], 'gi'), intraword2[x]);
     }
     return text;
 };
