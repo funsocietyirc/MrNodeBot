4f009cda2ef317629d05c82593207ab058dfe0e2
Dave Richer <dave@ir0ny.com>
2018-04-03 21:35:21 -0400

    add best guess

---
+++ scripts/commands/bestGuess.js
@@ -0,0 +1,34 @@
+const _ = require('lodash');
+const scriptInfo = {
+    name: 'Best Guess',
+    desc: 'Best attempt to match a nickname',
+    createdBy: 'IronY',
+};
+const gen = require('../generators/_nickBestGuess');
+
+module.exports = (app) => {
+    const bg = async (to, from, text, message) => {
+        const [nick] = text.split(' ');
+        if (!nick) {
+            app.say(to, `You are going to have to give me something to work with ${from}`);
+            return;
+        }
+
+        const result = await gen(nick);
+        if(_.isEmpty(result) || _.isEmpty(result.nearestNeighbor) || _.isNull(result.nearestNeighbor.probability)) {
+            app.say(to, `I got nothing, ${from}`);
+            return;
+        }
+
+        app.say(to, `There is a ${(result.probability * 100).toFixed(2)}% chance you are looking for ${result.nearestNeighbor.from}, ${from}`);
+    };
+
+    app.Commands.set('bg', {
+        desc: '[Nick] Best attempt to match a nickname',
+        access: app.Config.accessLevels.identified,
+        call: bg,
+    });
+
+    // Return the script info
+    return scriptInfo;
+};
---
+++ scripts/generators/_nickBestGuess.js
@@ -0,0 +1,42 @@
+const nn = require('nearest-neighbor');
+const logger = require('../../lib/logger');
+const Models = require('funsociety-bookshelf-model-loader');
+
+module.exports = async (nick) => {
+    // No Database
+    if (!Models.Logging) throw new Error('Database not available');
+    // Invalid Arguments
+    if (!nick) throw new Error('Nick is a required argument');
+
+    try {
+        const results = await Models.Logging.query((qb) => {
+            qb
+                .distinct('from')
+                .select('from');
+        }).fetchAll();
+
+        // No results
+        if (!results.length) return {};
+
+        return new Promise((res, rej) => {
+            nn.findMostSimilar({
+                from: nick,
+            }, results.toJSON(), [
+                { name: "from", measure: nn.comparisonMethods.word },
+            ], function(nearestNeighbor, probability) {
+                res({
+                    nearestNeighbor,
+                    probability,
+                });
+                console.log(nearestNeighbor);
+                console.log(probability);
+            });
+        });
+    } catch (err) {
+        logger.error('Something went wrong in the _getCandidatePopularityRanking file', {
+            message: err.message || '',
+            stack: err.stack || '',
+        });
+        throw new Error('Something went wrong getting the ranking');
+    }
+};
