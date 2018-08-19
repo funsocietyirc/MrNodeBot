249299c1d8660a51e214eec6d363c6ab3e641265
Dave Richer <dave@ir0ny.com>
2018-08-19 07:02:45 -0400

    - remove empty _getLuckStats - clean up and comment stats.js

--- scripts/admin/stats.js
+++ scripts/admin/stats.js
@@ -20,6 +20,7 @@ module.exports = (app) => {
             if (!app.Commands.get(key)[2]) app.say(from, `${key} : ${value}`);
         }),
     });
+
     // Get the bots uptime
     app.Commands.set('uptime', {
         desc: 'Get the current uptime',
@@ -27,26 +28,33 @@ module.exports = (app) => {
         call: (to, from, text, message) => app.say(to, `I have been alive since ${helpers.StartTime}, about ${helpers.StartTime.fromNow()}`),
     });
 
+    // Get a users high and low usage bound to a channel
     app.Commands.set('high-low-usage', {
         desc: '[user] Get a users high/low usage for a specified channel (defaults to current channel)',
         access: app.Config.accessLevels.admin,
         call: async (to, from, text, message) => {
             try {
+                // Extract Results
                 const [nick, preChannel] = text.split(' ');
+                // Extract channel or set default
                 const channel = preChannel || to;
 
+                // Validate nick
                 if (!nick) {
                     app.say(to, `I require a nick to fetch that data, ${from}`);
                     return;
                 }
 
+                // Fetch results
                 const results = await usageOverTime(channel, nick);
 
+                // Validate basic object
                 if (!_.isObject(results) || _.isEmpty(results)) {
                     app.say(to, `I am sorry, I was unable to find any data on ${nick} from ${channel}, ${from}`);
                     return;
                 }
 
+                // Validate results object
                 if (
                     !_.isObject(results.lowest) ||
                     !_.isSafeInteger(results.lowest.messages) ||
@@ -56,15 +64,15 @@ module.exports = (app) => {
                     !_.isDate(results.highest.timestamp)
 
                 ) {
-                    app.say(to, `I am sorry, something went wrong fetching data on ${nick} from ${channel}, ${from}`);
-                    logger.error('Something went wrong fetching the highest / lowest data', results);
+                    app.say(to, `I am sorry, I cannot find data on ${nick} from ${channel}, ${from}`);
+                    logger.debug('Something went wrong fetching the highest / lowest data', results);
                     return;
                 }
 
+                // Report back
                 app.say(to,
                     `${nick} was most active on ${channel} ${moment(results.highest.timestamp).format('MMMM Do, YYYY')} with a total of ${results.highest.messages} ${helpers.Plural('line', results.highest.messages)}, they were least active on ${moment(results.lowest.timestamp).format('MMMM Do, YYYY')} with a total of ${results.lowest.messages} ${helpers.Plural('line', results.lowest.messages)}`
                 );
-                console.dir(results);
             } catch (err) {
                 app.say(to, `I am sorry ${from}, something went wrong fetching usage`);
                 logger.error(`Error in usage command of _stats.js`, {
