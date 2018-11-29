ecce8dab768b618b73f85b66fb7d5f759c60d5c0
Dave Richer <dave@ir0ny.com>
2018-11-28 21:28:29 -0500

    deps

--- scripts/generators/_randomWebline.js
+++ scripts/generators/_randomWebline.js
@@ -5,10 +5,21 @@ const bofh = require('../generators/_bofhExcuse');
 const shower = require('../generators/_showerThoughts');
 const fml = require('../generators/_fmlLine');
 const advice = require('../generators/_getAdviceSlip');
+const chuck = require('../generators/_getChuckNorris');
+const til = require('../generators/_tilLine');
+const tifu = require('../generators/_tifuLine');
 
 module.exports = async () => {
     try {
-        const results = await _.sample([bofh, shower, fml, advice])();
+        const results = await _.sample([
+            bofh,
+            shower,
+            fml,
+            advice,
+            chuck,
+            til,
+            tifu,
+        ])();
         return _.isArray(results) ? _(results).first() : results;
     } catch (err) {
         logger.error('Something went wrong in the _randomWebline file', {
