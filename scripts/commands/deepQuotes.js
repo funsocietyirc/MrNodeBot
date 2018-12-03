91ab32dedea81b4395b35f30d03b325b499e29e1
Dave Richer <dave@ir0ny.com>
2018-12-03 04:04:18 -0500

    deep quotes, by Jack handy

---
+++ scripts/commands/deepQuotes.js
@@ -0,0 +1,17 @@
+const gen = require('../generators/_deepQuote');
+
+const scriptInfo = {
+    name: 'deepQuotes',
+    desc: 'Deep Quotes by Jack Handy',
+    createdBy: 'IronY',
+};
+
+module.exports = (app) => {
+    app.Commands.set('deepquote', {
+        desc: 'Deep Quotes By Jack Handy',
+        access: app.Config.accessLevels.identified,
+        call: (toe) =>  app.say(to, `${gen()} -- By Jack Handy`),
+    });
+    // Return the script info
+    return scriptInfo;
+};
