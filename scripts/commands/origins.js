2f2b2c48199be7fb4c8af643b7d5cca41ce5d890
Dave Richer <dave@ir0ny.com>
2018-12-17 03:11:40 -0500

    deps

--- scripts/commands/origins.js
+++ scripts/commands/origins.js
@@ -3,7 +3,52 @@ const scriptInfo = {
     desc: 'Show the Bots current up-time and other statistics',
     createdBy: 'IronY',
 };
+
 const helpers = require('../../helpers');
+
+/**
+ * Get Origins
+ * @param to
+ * @param from
+ * @param app
+ */
+const getOrigins = (to, from, app) => {
+    // Grab current up-time in hours
+    const procUptime = process.uptime() / 60 / 60;
+
+    // Get procText For it
+    const procText = () => {
+        if (procUptime < 1) return 'of an hour';
+        if (procUptime === 1) return 'hour';
+        if (procUptime > 1) return 'hours';
+    };
+
+    // Build String
+    const additionalAdmins = app.Admins.length > 1 ?
+        ` but I also listen to ${app.Admins.length - 1} ${helpers.Plural('other', app.Admins.length)}.` : '.';
+    const ignoreList = app.Ignore.length ?
+        `I also ignore ${app.Ignore.length} ${helpers.Plural('moron', app.Ignore.length)}, but that is a different story.` : '';
+    const uptimeText = `${procUptime.toFixed(2)} ${procText()}`;
+
+    // Final output
+    const out = `I am ${app._ircClient.nick}, I am currently running at version ${app.Config.project.version}. ` +
+        `${app.Config.owner.nick} is my master${additionalAdmins} ` +
+        `${ignoreList} My body is ${process.arch}, but my mind is ${process.platform}. This iteration of myself has been alive for ${uptimeText}. ` +
+        'I am open source and if you know JavaScript and NodeJS you should totally help make me better. ' +
+        `You can learn all about me at ${app.Config.project.repository.url}, or, better yet, contact me directly at ${app.Config.project.author} `;
+
+    // Say
+    app.say(from, out);
+    if (to !== from) app.say(to, `I have private messaged you my origin story, ${from}`);
+};
+
+/**
+ * Get the version
+ * @param to
+ * @param from
+ * @param app
+ */
+const getVersion = (to, from, app) => app.say(to, `My state of mind is ${app.Config.project.version}, ${from}. `);
 
 // Provide users with a brief origins story
 // Commands: origins
@@ -11,35 +56,13 @@ module.exports = (app) => {
     app.Commands.set('origins', {
         desc: 'My origin story',
         access: app.Config.accessLevels.guest,
-        call: (to, from, text, message) => {
-            // Grab current up-time in hours
-            const procUptime = process.uptime() / 60 / 60;
+        call: (to, from) => getOrigins(to, from, app),
+    });
 
-            // Get procText For it
-            const procText = () => {
-                if (procUptime < 1) return 'of an hour';
-                if (procUptime === 1) return 'hour';
-                if (procUptime > 1) return 'hours';
-            };
-
-            // Build String
-            const additionalAdmins = app.Admins.length > 1 ?
-                ` but I also listen to ${app.Admins.length - 1} ${helpers.Plural('other', app.Admins.length)}.` : '.';
-            const ignoreList = app.Ignore.length ?
-                `I also ignore ${app.Ignore.length} ${helpers.Plural('moron', app.Ignore.length)}, but that is a different story.` : '';
-            const uptimeText = `${procUptime.toFixed(2)} ${procText()}`;
-
-            // Final output
-            const out = `I am ${app._ircClient.nick}, I am currently running at version ${app.Config.project.version}. ` +
-                `${app.Config.owner.nick} is my master${additionalAdmins} ` +
-                `${ignoreList} My body is ${process.arch}, but my mind is ${process.platform}. This iteration of myself has been alive for ${uptimeText}. ` +
-                'I am open source and if you know JavaScript and NodeJS you should totally help make me better. ' +
-                `You can learn all about me at ${app.Config.project.repository.url}, or, better yet, contact me directly at ${app.Config.project.author} `;
-
-            // Say
-            app.say(from, out);
-            if (to !== from) app.say(to, `I have private messaged you my origin story, ${from}`);
-        },
+    app.Commands.set('version', {
+        desc: 'My Version',
+        access: app.Config.accessLevels.guest,
+        call: (to, from) => getVersion(to, from, app),
     });
 
     return scriptInfo;
