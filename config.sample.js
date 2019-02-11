c0f33a509823d5b1a3b1a27e589721751cd36c3a
Dave Richer <dave@ir0ny.com>
2019-02-11 01:18:28 -0500

    add config sample

--- config.sample.js
+++ config.sample.js
@@ -111,6 +111,16 @@ module.exports = {
         messageSplit: 512,
         encoding: 'utf8',
     },
+    // Mosca configuration
+    mqtt: {
+        enabled: true,
+        port: 1884,
+        backend: {
+            type: 'mongo',
+            url: 'mongodb://127.0.0.1:27017/mrnodebot',
+            pubsubCollection: 'mrnodebot',
+        }
+    },
     // Knex configuration http://knexjs.org/#Installation-client
     knex: {
         enabled: true,
