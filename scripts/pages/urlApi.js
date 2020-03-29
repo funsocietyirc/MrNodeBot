3fccb08641092ea89f8602b81ba1bfb31f6ab980
Dave Richer <dave@ir0ny.com>
2020-03-29 05:46:36 -0400

    Add channels to coronalinks

--- scripts/pages/urlApi.js
+++ scripts/pages/urlApi.js
@@ -74,6 +74,14 @@ module.exports = (app) => {
                         init = true;
                     }
 
+                    if (req.query.channels) {
+                        const channels = req.query.channels.split(',');
+                        qb.orWhere(function () {
+                            _.each(channels, channel => {
+                                this.where('to', channel.replace(hashPattern,'#'));
+                            })
+                        });                    }
+
                     // If there is a from in the query string
                     if (req.query.user) {
                         qb[getWhere()]('from', req.query.user);
--- web/vue/coronalinks.vue
+++ web/vue/coronalinks.vue
@@ -171,7 +171,7 @@
             },
             fetchData: function () {
                 let vm = this;
-                fetch('/api/urls?channel=%23%23coronavirus&pageSize=100').then(response => response.json()).then( (data) => {
+                fetch('/api/urls?channels=%23%23coronavirus,%23coronavirus,%23%23covid-19&pageSize=100').then(response => response.json()).then( (data) => {
                     vm.results = data.results;
                 }).catch(e => {
                     // TODO handle this
